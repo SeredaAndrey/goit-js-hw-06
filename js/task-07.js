1fe5b009502fc610c925e1a2d2a61a76197bdd96
Anrii Sereda <102906498+SeredaAndrey@users.noreply.github.com>
2022-09-15 12:57:25 +0300

    321

--- js/task-07.js
+++ js/task-07.js
@@ -2,9 +2,7 @@ const inputRange = document.querySelector("#font-size-control");
 // const inputRange = document.getElementById("font-size-control");
 const textSpan = document.querySelector("#text");
 // const textSpan = document.getElementById("text");
-console.dir(textSpan);
 
 inputRange.addEventListener("change", () => {
-  console.log(inputRange.value);
   textSpan.style.fontSize = `${inputRange.value}px`;
 });
