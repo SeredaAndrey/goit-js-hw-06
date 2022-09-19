d5edb851742a615d6f365fd213c8fc348d18efc6
Anrii Sereda <a.a.sereda@ukr.net>
2022-09-19 19:38:38 +0300

    fixed

--- js/task-05.js
+++ js/task-05.js
@@ -4,5 +4,9 @@ const output = document.querySelector("#name-output");
 input.addEventListener("input", onInputChange);
 
 function onInputChange(event) {
+  if (!event.currentTarget.value) {
+    output.textContent = "Anonymous";
+    return;
+  }
   output.textContent = event.currentTarget.value;
 }
--- js/task-07.js
+++ js/task-07.js
@@ -3,6 +3,6 @@ const inputRange = document.querySelector("#font-size-control");
 const textSpan = document.querySelector("#text");
 // const textSpan = document.getElementById("text");
 
-inputRange.addEventListener("change", () => {
+inputRange.addEventListener("input", () => {
   textSpan.style.fontSize = `${inputRange.value}px`;
 });
